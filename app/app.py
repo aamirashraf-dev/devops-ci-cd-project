515ebdbb13382d9f0e11526acfa3bce329410934
aamirashraf-dev <aamir.ashraf.dev@gmail.com>
2026-06-02 21:50:43 +0200

    Add Redis integration and Docker Compose setup

--- app/app.py
+++ app/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+
+redis_client = redis.Redis(host="redis", port=6379)
 
 
 @app.route("/")
 def home():
-    return {"message": "CI/CD Pipeline Working!"}
+    redis_client.incr("visits")
+    visits = redis_client.get("visits").decode("utf-8")
+
+    return {
+        "message": "CI/CD Pipeline Working!",
+        "visits": visits
+    }
 
 
 if __name__ == "__main__":
